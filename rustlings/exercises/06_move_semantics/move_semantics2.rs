dfa6345b7127010184bc9b6767c062dbe64c37f6
Jahysama <megametagross@outlook.de>
2025-02-18 00:25:02 +0500

    remove oopsie .devenv

--- rustlings/exercises/06_move_semantics/move_semantics2.rs
+++ rustlings/exercises/06_move_semantics/move_semantics2.rs
@@ -1,8 +1,12 @@
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+    // Parameter 'vec' takes ownership of the passed vector
+    // We shadow the immutable 'vec' with a mutable version
     let mut vec = vec;
 
+    // Modify the vector we own
     vec.push(88);
 
+    // Return ownership of the modified vector to the caller
     vec
 }
 
@@ -14,15 +18,19 @@ fn main() {
 mod tests {
     use super::*;
 
-    // TODO: Make both vectors `vec0` and `vec1` accessible at the same time to
-    // fix the compiler error in the test.
+    // This test will fail because of ownership rules!
     #[test]
     fn move_semantics2() {
+        // Create vec0 - we own it here
         let vec0 = vec![22, 44, 66];
 
-        let vec1 = fill_vec(vec0);
+        // vec0's ownership is moved to fill_vec's parameter
+        // After this line, vec0 is no longer valid in this scope!
+        let vec1 = fill_vec(vec0.clone());
 
-        assert_eq!(vec0, [22, 44, 66]);
+        // This will fail - vec0's ownership was moved
+        assert_eq!(vec0, [22, 44, 66]); // ❌ Error: vec0 was moved
+                                        // This works - we own vec1 now
         assert_eq!(vec1, [22, 44, 66, 88]);
     }
 }
